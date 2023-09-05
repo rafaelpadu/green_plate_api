b8eafe55e5d17a8add0eae3a9f18aa9429f7239a
Rafael de Padua <rafaelpadua07@gmail.com>
2023-09-04 20:42:10 -0400

    first commit

---
+++ src/main/java/com/green/plate/greenplateapi/GreenPlateApiApplication.java
@@ -0,0 +1,13 @@
+package com.green.plate.greenplateapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenPlateApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenPlateApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/green/plate/greenplateapi/GreenPlateApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.plate.greenplateapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenPlateApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
